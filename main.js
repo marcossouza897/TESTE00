88dfff1fb294c21bea4d2749df5875965307f695
yagojustooliveira <yagodoj@gmail.com>
2018-10-24 12:22:00 -0300

    erro corrigido com sucesso

--- main.js
+++ main.js
@@ -1,21 +1,24 @@
 var imagem = document.querySelectorAll('img');
 
 imagem[0].onclick = function(){
-var meulocal = imagem[0].getAttribute('src');
+    var meuLocal = imagem[0].getAttribute('src');
 
-    if(meulocal === 'fotos/primeira.png'){
-        imagem[0].setAttribute('src','fotos/primeira.jpg')
-    }else{
-        imagem[0].setAttribute('src','fotos/segunda.jpg')
+    if (meuLocal === 'fotos/primeira.jpg'){
+        imagem[0].setAttribute('src','fotos/terceira.jpg')
+    }
+    else{
+    imagem[0].setAttribute('src','fotos/primeira.jpg')
     }
 }
 
-imagem[1].onclick = function(){
-var meulocal = imagem[1].getAttribute('src');
 
-    if(meulocal === 'fotos/terceira.png'){
-        imagem[1].setAttribute('src', 'fotos/terceiras.jpg')
-    }else{
-        imagem[1].setAttribute('src', 'fotos/quarta.jpg') 
+imagem[1].onclick = function(){
+    var meuLocal = imagem[1].getAttribute('src');
+
+    if (meuLocal === 'fotos/segunda.jpg'){
+        imagem[1].setAttribute('src','fotos/quarta.jpg')
+    }
+    else{
+    imagem[1].setAttribute('src','fotos/segunda.jpg')
     }
 }
